0e0dfba5932d91833885a0908f546c71eba3f721
Abhishek Kumar <115116493+developerabhi04@users.noreply.github.com>
2023-07-30 18:37:12 +0530

    hello
    ddddddddd

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import errorMiddleware from "./middleware/error.js"
+
+// Route Imports
+import user from "./routes/userRoutes.js";
+
+dotenv.config({ path: "backend/config/config.env" })
+
+export const app = express();
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies (for forms with   
+
+
+// Route
+app.use("/api/v1", user);
+
+
+// middleware for error
+app.use(errorMiddleware)
+
+
---
+++ backend/controllers/userController.js
@@ -0,0 +1,102 @@
+import ErrorHander from "../utils/errorhander.js";
+import User from "../models/userModel.js";
+import sendToken from "../utils/jwtToken.js";
+
+
+
+
+// Register a user
+export const registerUser = async (req, res, next) => {
+
+    const {
+        firstName,
+        lastName,
+        email,
+        password,
+        phone } = req.body;
+
+    const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+        phone
+    });
+
+    // const token = user.getJWTToken();
+    // // console.log(token);
+
+    // res.status(201).json({
+    //     success: true,
+    //     token,
+    // });
+
+    sendToken(user, 201, res);
+};
+
+
+// Login user
+export const loginUser = async (req, res, next) => {
+
+    const { email, password } = req.body;
+
+    // checking if user has given password and email both.
+
+    if (!email || !password) {
+        return next(new ErrorHander("Please Enter Email & Password", 400));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new ErrorHander("Invalid email or password", 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHander("Invalid email or password", 401));
+    }
+
+    // const token = user.getJWTToken();
+    // console.log(token);
+
+    // res.status(200).json({
+    //     success: true,
+    //     token,
+    // }); 
+
+    sendToken(user, 200, res);
+}
+
+
+// Logout User
+export const logout = async (req, res, next) => {
+
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out",
+    });
+}
+
+
+
+// get all users 
+export const getAllUsers = async (req, res, next) => {
+    const users = await User.find();
+
+    res.status(200).json({
+        success: true,
+        users,
+    });
+}
+
+
+
+
+
